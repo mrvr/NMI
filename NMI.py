4a09b1ab74f169e4e44578597976233c59b26910
Unknown <raghu8linux@gmail.com>
2018-03-13 17:27:16 +0530

    modified functions for computing index.
    Run time rrors

--- NMI.py
+++ NMI.py
@@ -19,29 +19,40 @@ print('rawdata =\n',datatable)
 data = prepare_numeric_data(datatable)
 
 cats = Categorical([True,False,'+','-','yes','YES','Yes','no','NO','No'])
+
+#print('M Frame values')
+#print(M.iloc[0:,0:])
 # M is the subset of S having records without missing values in the nth column
 M = filter_mv_decisions(data)
 print("\n\ndata without mv in decision column")
 print(M)
-#print('M Frame values')
-#print(M.iloc[0:,0:])
 
 #compute idices
 #print("\nid(M) before =",id(M))
-IC = compute_indices(pd.DataFrame(M))
-print("\n IC =\n",IC)
-#compute the indices
 
+#get all mv records
+mvrecords = get_MV_records(M)
+#iterate through MV records and compute missing value
+non_mv_records = strip_MV_Records(M)
 
-dmatrix = compute_distance(list(IC))
+#print(pd.DataFrame(mvrecords))
 
-mv_record_index_list = [0,]
-#dmatrix = [[0,0.99,1.11,1.46],[0.99,0,2.11,2.27],[1.11,2.11,0,2.32],[1.46,2.27,2.32,0]]
-dmatrix = [[0,0.86,0.99],[0.86,0,1.32],[0.99,1.32,0]]
-
-#dmatrix = []
-zdmatrix = compute_zd(dmatrix,mv_record_index_list,len(dmatrix))
-print("zd = ",zdmatrix)
+for i in mvrecords.index:
+    
+    IC = get_indices(pd.DataFrame(non_mv_records),pd.DataFrame(mvrecords.iloc[i,:]))
+    print("\n IC =\n",IC)
+    #compute the indices
+    
+    
+    dmatrix = compute_distance(list(IC))
+    
+    mv_record_index_list = [0,]
+    #dmatrix = [[0,0.99,1.11,1.46],[0.99,0,2.11,2.27],[1.11,2.11,0,2.32],[1.46,2.27,2.32,0]]
+    #dmatrix = [[0,0.86,0.99],[0.86,0,1.32],[0.99,1.32,0]]
+    
+    #dmatrix = []
+    zdmatrix = compute_zd(dmatrix,mv_record_index_list,len(dmatrix))
+    print("zd = ",zdmatrix)
 
 #zdmatrix = compute_zd(dmatrix,1)
 #counter = 0
--- nmilib.py
+++ nmilib.py
@@ -40,6 +40,25 @@ def strip_MV_Records(Samples):
     #print("\n\n\n\ndata after stripping MV\n",temp)
     return (temp)
     
+def get_MV_records(Samples):
+    temp = pd.DataFrame(Samples)
+    temp_index = temp.index
+    #print("\ntemp.index = ",temp_index) 
+    for i in temp.index:    
+       
+        #print("\ntemp.loc[i] =",temp.loc[i])
+        nmv = (temp.loc[i]).isnull()
+        #print("\nnmv = \n",nmv)
+        #print("\nnmv.notnull() =",nmv.notnull())
+        #print("\ntemp[nmv] =",temp.loc[nmv])
+        if not nmv.any():
+            temp.drop(i,inplace=True)
+#        if nmv.any():
+#            temp.drop(temp.loc[i],axis=0,inplace=True)
+    
+    #print("\n\n\n\ndata after stripping MV\n",temp)
+    return (temp)
+
 
 #Recover numeric data
 def prepare_numeric_data(datatable):
@@ -162,25 +181,131 @@ def compute_indices(M):
         ICmaster.insert(l,pd.DataFrame(IC_i_list))
 
     return ICmaster
+
+def get_indices(M,N):
+    ic_panel = []    
+    IC=[]
+    #M=Samples = SNMV    
     
+    R =  M.index
+    C = M.columns
+    col_index = C.get_loc
+    
+    
+    #mark categorical element types       
+    
+    mv_row_index = []
+    mv_col_index = []  
+    non_mv_row_index = []
+    
+    
+    mv_col_index = [N.columns.get_loc]
+    mv_row_index = [N.index.get_loc]
+    
+#    for col in C: 
+#        # we do not need decision column here, we only need columns with missing attributes
+#        if col == C[-1]:
+#            break           
+#        for row in R:
+#            #find the index of missing row and col and store in mv_row and mv_col
+#            if (pd.isnull((M[col][row]))) :
+#                mv_col_index.append(C.get_loc(col)) 
+#                mv_row_index.append(R.get_loc(row))
+    
+    for row in R:
+        non_mv_row_index.append(R.get_loc(row))
+            
+    print("\nnon_mv_row_index\n",non_mv_row_index)               
+    print("\n\nmv_row_index=",mv_row_index)
+    print("\n\nmv_col_index =",mv_col_index)
+    
+    dec_class_yes  = M[M[C[-1]].isin(['+', 'yes', 'YES' , 1,True])]
+    
+    print("\n\ndec_class_yes = ",dec_class_yes)    
+    
+    dec_class_no  = M[M[C[-1]].isin(['-' , 'no' , 'NO' , 'No',0,False])]
+    print("\n\ndec_class_no = ",dec_class_no)
+    
+    #calculate IC index based on column having the missing value
+    atrib_type  = 0 # will be decided in the loop    
+    ICmaster = [] #IC is a 3d panda structure containing values
+    
+    for l in  mv_col_index:        
+        IC_i_list = []
+        vals = M[C[l][:]]
+        val_exists = vals.isin([True,False,'+','-','yes','YES','Yes','no','NO','No'])
+        #for i in mv_row_index:
+        #As per NMI we need to strip all tuples with MV while calculating IC
+        for i in non_mv_row_index:
+            IC_k_list = []
+            
+            for k in non_mv_row_index: 
+                
+                last_col_index = C.get_loc(C[-1])
+                print("\nlast_col_index=",last_col_index)
+                I = M.iloc[i,last_col_index]
+                K = M.iloc[k,last_col_index]
+                print("\nI =",I," K=",K)
+                
+                                    
+                #if i and k belongs to same decision class                
+                if I == K: #same decision  class
+                   print("\n\ncase-1:same decision class")  
+                   #IC for same record is 0                
+                   if i==k:
+                        print("RECORDS ARE SAME, IC =0")
+                        IC_k_list.insert(k,0)
+                   #group the elements into related class                  
+                   
+                   #print("\n\nvals =\n",val_exists.any())       
+                   elif val_exists.any():                       
+                       print('\n\ncase-1:categorical')
+                       ic_val = compute_case1_categorical(pd.DataFrame(M),C,l,i,k,non_mv_row_index)
+                       IC_k_list.insert(k,ic_val)
+                  
+                   elif (M[C[l]].dtypes == "float64"):
+                       print('\n\ncase-1:type is float')
+                       ic_val = compute_case1_fractions(pd.DataFrame(M),C,l,i,k,non_mv_row_index)
+                       IC_k_list.insert(k,ic_val)
+                   else:
+                       print('\n\ncase-1:tInput Error, column type could not be determined\n')
+                else: #i and k belongs to different decision class
+                    print("\n\ncase-2:Different decision class ")
+                    
+                    if val_exists.any():
+                        print('\n\ncase-2:categorical')
+                        ic_val = compute_case2_categorical(pd.DataFrame(M),C,l,i,k,non_mv_row_index)
+                        IC_k_list.insert(k,ic_val)
+                    
+                    elif (M[C[l]].dtypes == "float64"):
+                        print('\n\ncase-2:type is float')
+                        ic_val = compute_case2_fractions(pd.DataFrame(M),C,l,i,k,non_mv_row_index)
+                        IC_k_list.insert(k,ic_val)
+                    else:
+                        print('\n\ncase-2:Input Error, column type could not be determined\n')
+                        
+            IC_i_list.insert(i,pd.Series(IC_k_list))  
+        ICmaster.insert(l,pd.DataFrame(IC_i_list))
+
+    return ICmaster
 
 def compute_distance(IC):
     
     
-    d= [[0,0.99,1.11,1.46],[0.99,0,2.11,2.27],[1.11,2.11,0,2.32],[1.46,2.27,2.32,0]]
-#    distance = []
-#    print('\n\nlenght of d =',len(d))
-#    d_ik = 0
-#    for i in IC:
-#        for j in i:
-#            for k in j:
-#                d_ik+=(k**2)
-#                        
-#            distance.append(d_ik**0.5)    
-#            print('\n\ndistance =',distance)
+    #d= [[0,0.99,1.11,1.46],[0.99,0,2.11,2.27],[1.11,2.11,0,2.32],[1.46,2.27,2.32,0]]
+    distance = []
+    print('\n\nlenght of d =',len(distance))
+    d_ik = 0
+    for i in IC:
+        for j in i:
+            for k in j:
+                d_ik+=(k**2)
+                        
+            distance.append(d_ik**0.5)    
+            print('\n\ndistance =',distance)
     
-    return d
-    #+return distance
+    #return d
+    return distance
     
     
 #computing Z(d) as per New Imputation Method
